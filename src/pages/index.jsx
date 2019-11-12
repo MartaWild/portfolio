4067d07eae1d56fe61afd164a4c84780b2b3e72a
Wild Potato <if-so-girl865@mail.ru>
2019-11-12 14:53:22 +0500

    New beginning

---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => {
+    return (
+        <div>
+            <div>Hello world!</div>
+            <a href="/page"> Перейти на страницу </a>
+        </div>
+    )
+}
