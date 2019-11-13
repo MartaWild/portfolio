6be467c739de7580c31c605009dfd40ba64f53e7
Wild Potato <if-so-girl865@mail.ru>
2019-11-13 16:05:37 +0500

    Header, main page wip

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,61 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 
 /*CSS*/
+
 const Image = styled.img`
-    width: 200px;
+    width: 100px;
+`;
+
+const LogoWrapper = styled.div`
+    margin-left: 20px;
+`;
+
+const NavMenu = styled.nav`
+    display: flex;
+    width: 550px;
+    /*width: 30%;*/
+    justify-content: space-between;
+    margin-right: 100px;
+    align-items: center;
+
+`;
+
+const HeaderBar = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 0px;
+    background-color: rgb(250, 250, 250);
+    font-family: 'Lobster', cursive;
+    font-size: 18px;
+    flex-basis: 0;
+    flex-grow: 0;
+`;
+
+const MenuLink = styled.a`
+    text-decoration: none;
+    color: rgb(26, 101, 147);
+    :active {
+        color: rgb(26, 101, 147);
+    }
 `;
 
 export default function Header(){
     return (
-        <div>
-            <a href="/" className="header__logo-container">
-                <Image src="/icon.png" className="header__logo-img" />
-            </a>
-        </div>
+        <HeaderBar>
+            <LogoWrapper>
+                <a href="/">
+                    <Image src="/icon.png" />
+                </a>
+            </LogoWrapper>
+
+            <NavMenu>
+                <MenuLink href="/"> Веб-разработка </MenuLink>
+                <MenuLink href="/"> Арт и дизайн </MenuLink>
+                <MenuLink href="/"> Контакты </MenuLink>
+                <MenuLink href="/"> Eng / Rus </MenuLink>
+            </NavMenu>
+
+        </HeaderBar>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,29 @@
-import React from "react"
-import Header from "../components/header"
+import React from "react";
+import Header from "../components/header";
+import styled from "styled-components";
 
-export default () => {
+/*CSS*/
+const MainPage = styled.div`
+    background-color: rgb(228, 237, 242);
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+`;
+
+const Content = styled.div`
+    flex-grow: 1;
+    flex-basis: 1;
+    padding: 20px;
+`;
+
+export default function Main(){
     return (
-        <div>
+        <MainPage>
             <Header />
-            <div>Hello world!</div>
-            <a href="/page"> Перейти на страницу </a>
-        </div>
+            <Content>
+                Конь  Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver. Looking down into the dark gulf below, I could see a ruddy light streaming through a rift in the clouds.  Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver. Looking down into the dark gulf below, I could see a ruddy light streaming through a rift in the clouds.  Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle. By the same illusion which lifts the horizon of the sea to the level of the spectator on a hillside, the sable cloud beneath was dished out, and the car seemed to float in the middle of an immense dark sphere, whose upper half was strewn with silver. Looking down into the dark gulf below, I could see a ruddy light streaming through a rift in the clouds. 
+            </Content>
+            
+        </MainPage>
     )
 }
